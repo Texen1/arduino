4a8b2a9b30d7bcd48128c9a2fa42a1e24559eac5
Jason Le <jasonlongle@gmail.com>
2022-02-16 18:19:54 -0800

    Explored lights and sound functions.

--- HelloAdi/HelloAdi.ino
+++ HelloAdi/HelloAdi.ino
@@ -12,18 +12,27 @@ version 2.1 of the License, or (at your option) any later version.
 */
 
 #include "Arduboy2.h"
+#include "ArduboyTones.h"
 
 Arduboy2 arduboy;
+ArduboyTones sound(arduboy.audio.enabled);
 
-void setup() {
-  arduboy.begin();
-  arduboy.setFrameRate(15);
-}
 int ballRadius = 3;
 int ballX = 1;
 int ballY = 1;
 float ballSpeedX = 4;
 float ballSpeedY = 4;
+
+void setup() {
+  arduboy.begin();
+  arduboy.setFrameRate(15);
+  //arduboy.digitalWriteRGB(RGB_OFF,RGB_ON,RGB_OFF);
+  //arduboy.digitalWriteRGB(BLUE_LED, RGB_ON);
+  arduboy.setRGBled(RGB_OFF,RGB_OFF, RGB_OFF);
+  arduboy.setRGBled(0, 0, 0);
+  sound.tone(440,1000);
+}
+
 
 void loop() {
   if (!(arduboy.nextFrame()))
@@ -60,18 +69,27 @@ void moveAllThings(){
   if(ballX < 0){
     //ballSpeedX = ballSpeedX * -1;
     ballX = arduboy.width();
+    arduboy.setRGBled(0, 255, 255);
+    sound.tone(700,200);
     //ballRadius += 1;
   }else if(ballX > arduboy.width()){
     ballX = 0;
+    arduboy.setRGBled(0,255,0);
+    sound.tone(700,200);
+  }else{
+    arduboy.setRGBled(0,0,0);
   }
 
   // ==== STOP ON EDGE FOR TOP AND BOTTOM EDGES =====
-  if(ballY < ballRadius){
+  if(ballY < ballRadius){                         // Check top collision
     //ballSpeedY = ballSpeedY * -1;
     ballY = ballRadius;
-  }
-  if (ballY>arduboy.height()- ballRadius){
+    arduboy.setRGBled(RED_LED,255);
+  }else if (ballY>arduboy.height()- ballRadius){ //if not top, check bottom
     ballY = arduboy.height() - ballRadius;
+    arduboy.setRGBled(RED_LED,255);
+  }else{
+    arduboy.setRGBled(RED_LED,0);                //if none of the above
   }
   
 }
