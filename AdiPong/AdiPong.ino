79ef6a09d2eb2d4e18ea2e25daeb056a44330f3d
Jason Le <jasonlongle@gmail.com>
2022-03-02 18:21:56 -0800

    add paddle and ball movement and collisions

--- AdiPong/AdiPong.ino
+++ AdiPong/AdiPong.ino
@@ -5,14 +5,15 @@ Arduboy2 arduboy;
 ArduboyTones sound(arduboy.audio.enabled);
 
 int ballRadius = 3;
-int ballX = arduboy.width()/2;
+int ballX = 0; //arduboy.width()/2;
 int ballY = arduboy.height()/2;
-float ballSpeedX = 4;
-float ballSpeedY = 4;
+float ballSpeedX = 2;
+float ballSpeedY = 2;
 
 int p1PaddleY = arduboy.height()/2;
 int p2PaddleY = arduboy.height()/2;
-
+int p1PaddleHeight = 12;
+int p2PaddleHeight = 8;
 void setup() {
   // put your setup code here, to run once:
   arduboy.begin();
@@ -35,10 +36,46 @@ void loop() {
 
 void handleInputs(){
   //Todo
+  if (arduboy.pressed(UP_BUTTON)){
+    if (p1PaddleY > p1PaddleHeight/2){
+      p1PaddleY -= 4;
+    }else{
+      p1PaddleY = p1PaddleHeight/2;
+    }
+    
+  }
+  if (arduboy.pressed(DOWN_BUTTON)){
+    if (p1PaddleY < arduboy.height() - p1PaddleHeight/2){
+      p1PaddleY += 4;
+    }else{
+      p1PaddleY = arduboy.height() - p1PaddleHeight/2;
+    }
+      
+  }
 }
 
 void moveAllThings(){
-  //Todo 
+  if (ballY < ballRadius || ballY > arduboy.height() - ballRadius){
+    ballSpeedY = -ballSpeedY;
+  }
+  if (ballX > arduboy.width() - ballRadius){
+    ballSpeedX = -ballSpeedX;
+  }
+
+  // if ballx and ballY match paddleX(4) and paddleY
+  // then reverse the ballX
+  if (ballX <= 0){
+    ballX = arduboy.width()/2;
+    ballSpeedX = -ballSpeedX;
+  }
+  else if(ballX <= 4 + 3 + ballRadius && 
+     ballY >= p1PaddleY - p1PaddleHeight/2 &&
+     ballY <= p1PaddleY + p1PaddleHeight/2){
+    ballSpeedX = -ballSpeedX;
+  }
+
+  ballX = ballX + ballSpeedX;
+  ballY = ballY + ballSpeedY;
 }
 
 void drawAllThings(){
@@ -58,5 +95,8 @@ void drawNet(){
 }
 
 void drawPaddles(){
-  arduboy.fillRect(4, 10, 3, 8, WHITE);
+  arduboy.fillRect(4, p1PaddleY - p1PaddleHeight/2,
+                   3, p1PaddleHeight, WHITE);
+  arduboy.fillRect(arduboy.width()-4, p2PaddleY - p2PaddleHeight/2,
+                   3 ,p2PaddleHeight, WHITE);
 }
